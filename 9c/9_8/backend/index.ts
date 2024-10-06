998c79ec7937e459e15fe7d94152dd5ba5d38f1c
Andrei D <codeandi@gmail.com>
2024-10-06 12:29:32 +0300

    updates

--- 9c/9_8/backend/index.ts
+++ 9c/9_8/backend/index.ts
@@ -2,15 +2,12 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 const cors = require('cors');
-app.use(cors());
+app.use(cors())
+
 
 const PORT =3000;
 
 app.get("/api/ping", (_req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain',
-        'Access-Control-Allow-Origin': '*'
-    });
     console.log('someone pinged here');
     res.send("pong");
 });
